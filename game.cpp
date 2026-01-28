714e89e773d721aac768c2d8f8b6a00f5c95446d
avrumhamlincoln <ahorsley2025@icloud.com>
2026-01-28 16:18:21 +0000

    added some more code

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
-#include <iostream>#include <random>
+#include <iostream>
+#include <random>
 
 void advance(int horseNum, int* horses);
 void printLane(int horseNum, int* horses);
@@ -11,6 +12,29 @@ coin = dist(rd);
 
 int main(){
 	int horses[] = {0, 0, 0, 0, 0};
-	bool keepGoing = true
+	bool keepGoing = true;
+
+	while(keepGoing){
+		for (int i = 0; i < 5; i++){
+			advance(i, horses);
+			printLane(i, horses);
+			if (isWinner(i, horses)){
+				std::cout << "Horse" << i << "Wins!" << std::endl;
+			}
+		}
+	
+	}
+
+}
+
+void advance(int horseNum, int* horses){
+	for(int i = 0; i < 5; i++){
+		if (coin  == 1){
+			// I am stuck on how to get this working
+		}
+	}
+}
+
+void printLane(int horseNum, int* horses){
 
 }
